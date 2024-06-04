014d4bcb22aa297d8055c670031ef93f97110da2
DanieloM83 <danielo83mail@gmail.com>
2024-06-04 17:10:39 +0300

    :sparkles: Tester functionality extension
    Minor logger module refactoring; Added a nice and more meaningful output of the results; Added a timer param.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 from os import get_terminal_size
-from time import time
+from time import time, sleep
 
 from colorama import Fore, Style, init
 from getch import getch
 
-from logger import Log
+import logger as Log
 from text import EnglishFile
 
 init(autoreset=False)
 
 
 class Tester:
-    def __init__(self, words: int = 200, offset: int = 0):
+    def __init__(self, words: int = 200, offset: int = 0, timer: int = 60):
         self.text = EnglishFile.get_text(words)
         self.size = len(self.text)
         self.words = words
+        self.timer = timer
+        self.correct = 0
         self.prev = []
         self.index = 0
         self._offset = offset
@@ -25,25 +27,42 @@ class Tester:
 
     def start(self):
         Log.warning("Make sure the command line is wide enough.")
+        sleep(1.5)
         Log.info("Words count:", self.words)
-
+        Log.info("Timer: {:.2f}s".format(self.timer) if self.timer else "Unlimited time.")
+        sleep(1)
         start_time = 0
         while self.index < self.size:
             print(f"\r{self.offset * 2 * ' '}\r" + self.underlay(), end="\r")
             print(self.overlay(), end="")
             self.check(getch())
             start_time = start_time or time()
+            if self.timer and time() - start_time >= self.timer: break
+
         print("\r" + self.offset * 2 * " ", end="\r")
-        Log.success("Your time:", time() - start_time, header="FINISH")
+        delta_time = min(time() - start_time, self.timer)
+        accurate = self.correct * 100 / self.index
+
+        Log.info("Test ended!")
+        sleep(1)
+        Log.warning("{:.1f} %".format(accurate), header="ACCURATE")
+        sleep(.5)
+        Log.success(int((self.correct + 1) * (60 / delta_time)), header="CPM", end="\t")
+        Log.success(int(((self.correct + 1) / 5) * (60 / delta_time)), header="WPM")
 
     def check(self, symbol: str):
         if symbol == "\x7f":
             if self.index != 0:
-                self.prev.pop()
+                self.correct -= (self.prev.pop() == (Fore.LIGHTGREEN_EX +
+                                                     self.text[self.index] +
+                                                     Style.RESET_ALL))
             self.index = max(0, self.index - 1)
             return
-        self.prev.append(
-            (Fore.LIGHTGREEN_EX if symbol == self.text[self.index] else Fore.LIGHTRED_EX) + symbol + Style.RESET_ALL)
+        if symbol == self.text[self.index]:
+            self.prev.append(Fore.LIGHTGREEN_EX + symbol + Style.RESET_ALL)
+            self.correct += 1
+        else:
+            self.prev.append(Fore.LIGHTRED_EX + symbol + Style.RESET_ALL)
         self.index += 1
 
     def underlay(self) -> str:
